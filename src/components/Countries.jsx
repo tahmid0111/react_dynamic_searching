b2017f29106ebee1322ea738e82fdd5e53aecca4
tahmid-emam <0.1tahmidemam@gmail.com>
2024-03-05 23:53:24 +0600

    trying to push in vercel

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -21,7 +21,7 @@ const Countries = () => {
 
   useEffect(()=> {
     fetch(`https://restcountries.com/v3.1/all`)
-          .then(res=>res.json())
+          .then(res=> res.json())
           .then(datas=> setCountries(datas))
           }, [])
   return (
